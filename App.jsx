a10ac6f4cd834cccbcadffc93cbc67f8ed27882f
Jordan <wild_hunter0806@hotmail.com>
2023-12-16 17:16:07 +0800

    Changed to javascript

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React, {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Tasks from './components/Tasks';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {
+        <View style={{flex: 1}}>
+          <Tasks />
+        </View>
+      }
+    </NavigationContainer>
+  );
+}
