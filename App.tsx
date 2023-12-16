a71a51868d8e0eebcdc7fc5f30ba88038f0ed10d
Jordan <wild_hunter0806@hotmail.com>
2023-12-16 16:29:15 +0800

    Added task components

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import React, {View, Text} from 'react-native';
 
+import Tasks from './components/Tasks';
+
 export default function App() {
   return (
-    <View>
+    <View style={{justifyContent:'center', alignContent:'center', height: '100%'}}>
       <Text>Hello World</Text>
+      <Tasks />
     </View>
   );
 }
+
